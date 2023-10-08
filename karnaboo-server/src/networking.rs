606601f7033aa1cd992e6d696141ae3d5cf41053
romzorus <romzorus591@gmail.com>
2023-10-08 23:04:07 +0200

    Fixing the request content : the ip of the client is now filled by the server since the client can't determine it itself

--- karnaboo-server/src/networking.rs
+++ karnaboo-server/src/networking.rs
@@ -7,7 +7,7 @@ You should have received a copy of the GNU General Public License along with thi
 */
 
 use crate::configuration::Networking;
-use crate::database::{self, NodeClient, NodeHostRequest};
+use crate::database::{self, NodeHostRequest, NodeClient, NodeDiss, NodeReps};
 use arangors::uclient::reqwest::ReqwestClient;
 use arangors::Database;
 use colored::Colorize;
@@ -46,12 +46,12 @@ pub async fn thread_networking(
     };
 
     // Buffer for hosts requests not answered yet
-    let mut request_handler: TcpStream;
+    let mut request_handler: &TcpStream;
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                request_handler = stream;
+                request_handler = &stream;
 
                 let mut buffer: [u8; 1024] = [0; 1024];
                 let size = request_handler
@@ -62,46 +62,78 @@ pub async fn thread_networking(
                     serde_json::from_str(&serialized_request_content)
                         .expect("Unable to deserialize data received from TcpStream");
 
-                // Inform user in real time about new request
+                /* It is necessary to deconstruct the received content, get the ip from TcpStream, then
+                reconstruct into another NodeHostRequest, because, when karnaboo agent sends its request to
+                the server, it doesn't know which network interface (meaning ip address) is going to be used
+                at lower level (routing table and everything) so it can't put itself its own ip address in
+                the request. The client's ip address has to be read from the outside aka the server.
+                So, when the server receives a request from an ip address, it has to put itself this ip in
+                the request in order for it to be complete. */
+
+                // Complete the request with the actual ip address of the client
+                // and alert user in real time about new request
+                let final_request_content: database::NodeHostRequest;
+
                 match &deserialized_request_content {
                     NodeHostRequest::Client(content) => {
                         println!(
                             "{}",
                             format!(
                                 "New request for client role from \'{}\' at {}",
-                                content.hostname, content.ip
+                                content.hostname, stream.local_addr().unwrap().ip()
                             )
                             .bold()
                             .blue()
                         );
+
+                        final_request_content = NodeHostRequest::Client(NodeClient {
+                            hostname: content.hostname.clone(),
+                            ip: stream.local_addr().unwrap().ip().to_string(), // Getting ip address from TcpStream
+                            osname: content.osname.clone(),
+                            osversion: content.osversion.clone(),
+                            hostid: content.hostid.clone() });
                     }
                     NodeHostRequest::Diss(content) => {
                         println!(
                             "{}",
                             format!(
                                 "New request for DISS role from \'{}\' at {}",
-                                content.hostname, content.ip
+                                content.hostname, stream.local_addr().unwrap().ip()
                             )
                             .bold()
                             .blue()
                         );
+
+                        final_request_content = NodeHostRequest::Diss(NodeDiss {
+                            hostname: content.hostname.clone(),
+                            ip: stream.local_addr().unwrap().ip().to_string(), // Getting ip address from TcpStream
+                            osname: content.osname.clone(),
+                            osversion: content.osversion.clone(),
+                            hostid: content.hostid.clone() });
                     }
                     NodeHostRequest::Reps(content) => {
                         println!(
                             "{}",
                             format!(
                                 "New request for REPS role from \'{}\' at {}",
-                                content.hostname, content.ip
+                                content.hostname, stream.local_addr().unwrap().ip()
                             )
                             .bold()
                             .blue()
                         );
+
+                        final_request_content = NodeHostRequest::Reps(NodeReps {
+                            hostname: content.hostname.clone(),
+                            ip: stream.local_addr().unwrap().ip().to_string(), // Getting ip address from TcpStream
+                            osname: content.osname.clone(),
+                            osversion: content.osversion.clone(),
+                            hostid: content.hostid.clone() });
                     }
                 }
-
+                
                 // Add this request to the waiting requests buffer
                 let mut waiting_requests = waiting_requests_buffer_networking.lock().await;
-                waiting_requests.push(deserialized_request_content);
+                waiting_requests.push(final_request_content);
             }
             Err(e) => {
                 eprintln!("Unable to establish TcpStream with remote host : {}", e);
