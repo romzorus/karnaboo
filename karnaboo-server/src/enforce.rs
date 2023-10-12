2e7b360a00ce3906841d980f76332793cbed316b
romzorus <romzorus591@gmail.com>
2023-10-12 14:31:24 +0200

    Work on enforce() and enforce_specific_host() : server can now send the appropriate (generic for now) scripts to the hosts for execution

--- karnaboo-server/src/enforce.rs
+++ karnaboo-server/src/enforce.rs
@@ -10,7 +10,7 @@ use std::process::exit;
 
 
 
-use crate::database::NodeHostRequest;
+use crate::database::{NodeHostRequest, NodeReps, NodeDiss, NodeClient};
 use crate::configuration::DatabaseInfo;
 use crate::database::NodeOs;
 
@@ -29,21 +29,62 @@ pub async fn enforce(db_info: &DatabaseInfo) {
 
     let db_connector = db_connection.db(&db_info.db_name).await.unwrap();
 
-    // Testing part
-    println!("Enforcing the database on the hosts :");
-    get_script_from_db(db_connector, "175779", &"diss".to_string()).await.unwrap();
+    // Starting with the REPS
+    println!("Enforcing the REPS...");
+    let reps_list: Vec<NodeReps> = db_connector
+        .aql_str("FOR host IN reps RETURN host")
+        .await
+        .unwrap();
 
-    let reps_list: Vec<String> = vec![];
+        for reps in reps_list.into_iter() {
+        let exec_return = enforce_specific_host(&db_connector, reps._key.as_str(), "reps", format!("{}:9016", reps.ip).parse().unwrap()).await;
+        println!("----------------");
+        println!("  ** Host : {} at {}", reps.hostname, reps.ip);
+        println!("  ** Exec return :");
+        println!("{}", exec_return.stdout);
+        println!("");
+    }
 
-    // for host in reps {
-        // enforce_specific_host(host...etc)
-    // }
+    // Pursuing with the DISS
+    println!("Enforcing the DISS...");
+    let diss_list: Vec<NodeDiss> = db_connector
+        .aql_str("FOR host IN diss RETURN host")
+        .await
+        .unwrap();
+
+        for diss in diss_list.into_iter() {
+        let exec_return = enforce_specific_host(&db_connector, diss._key.as_str(), "diss", format!("{}:9016", diss.ip).parse().unwrap()).await;
+        println!("----------------");
+        println!("  ** Host : {} at {}", diss.hostname, diss.ip);
+        println!("  ** Exec return :");
+        println!("{}", exec_return.stdout);
+        println!("");
+    }
+
+    // Ending with the clients
+    println!("Enforcing the clients...");
+    let client_list: Vec<NodeClient> = db_connector
+        .aql_str("FOR host IN clients RETURN host")
+        .await
+        .unwrap();
+
+        for client in client_list.into_iter() {
+        let exec_return = enforce_specific_host(&db_connector, client._key.as_str(), "client", format!("{}:9016", client.ip).parse().unwrap()).await;
+        println!("----------------");
+        println!("  ** Host : {} at {}", client.hostname, client.ip);
+        println!("  ** Exec return :");
+        println!("{}", exec_return.stdout);
+        println!("");
+    }
+
 }
 
-pub async fn enforce_specific_host(host: NodeHostRequest, host_socket: SocketAddr, role: String, db_connector: Database<ReqwestClient>, host_key: &str) -> ExecutionResult {
+pub async fn enforce_specific_host(db_connector: &Database<ReqwestClient>, host_key: &str, role: &str, host_socket: SocketAddr) -> ExecutionResult {
+    println!("  - Enforcing a {} at {}", role, host_socket.ip());
+
     // 1. Get the appropriate script
     let mut generic_instructions = FinalInstructions { script_content: String::from("") };
-    match get_script_from_db(db_connector, host_key, &role).await {
+    match get_script_from_db(&db_connector, host_key, &role).await {
         Ok(content) => {
             generic_instructions = content;
         }
@@ -55,7 +96,7 @@ pub async fn enforce_specific_host(host: NodeHostRequest, host_socket: SocketAdd
     };
 
     // 2. Adapt the script to the specific topology of the host
-    let final_instructions = adapt_instruction(host, role, generic_instructions);
+    let final_instructions = adapt_instruction( role, generic_instructions);
 
     // 3. Send this script to the host
     send_script_to_host(host_socket, final_instructions);
@@ -71,15 +112,17 @@ pub async fn enforce_specific_host(host: NodeHostRequest, host_socket: SocketAdd
 
 pub fn wait_for_host_exec_return() -> ExecutionResult {
     // Do something
+    println!("WAIT_FOR_EXEC_RETURN : TESTING PHASE");
     ExecutionResult { exit_status: String::from(""), stdout: String::from(""), stderr: String::from("") }
 }
 
-pub fn adapt_instruction(host: NodeHostRequest, role: String, generic_instructions: FinalInstructions) -> FinalInstructions {
+pub fn adapt_instruction(role: &str, generic_instructions: FinalInstructions) -> FinalInstructions {
     // Do something
-    FinalInstructions { script_content: String::from("") }
+    println!("ADAPT_INSTRUCTION : TESTING PHASE");
+    generic_instructions
 }
 
-pub async fn get_script_from_db(db_connector: Database<ReqwestClient>, host_key: &str, role: &str) -> Result<FinalInstructions, String> {
+pub async fn get_script_from_db(db_connector: &Database<ReqwestClient>, host_key: &str, role: &str) -> Result<FinalInstructions, String> {
     // Go from host to its connected OS then to the appropriate script connected to this OS
 
     // Which OS the host is connected to ?
@@ -99,19 +142,15 @@ pub async fn get_script_from_db(db_connector: Database<ReqwestClient>, host_key:
             host_os[0], role).as_str())
         .await
         .unwrap();
-
-        // println!("{}", format!("FOR link IN script_compatible_with FILTER link._to == \"{}\" FILTER document(link._from).role == \"{}\" RETURN document(link._from).content",
-        // host_os[0], role).as_str());
     }
     
     if script_result.len() != 1 {
-        println!("This OS is connected to none or too much scripts for this role.");
-        println!("Script_result = {:?}", script_result);
+        Err(format!("OS connected to none or too much scripts. Script_result = {:?}", script_result))
     } else {
-        println!("Script final : {}", script_result[0]);
+        Ok(FinalInstructions { script_content: script_result[0].clone()})
     }
 
-    Ok(FinalInstructions { script_content: script_result[0].clone()})
+    
 }
 
 // This function is used to fulfill the database.
@@ -168,7 +207,7 @@ pub struct FinalInstructions {
     pub script_content: String
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct ExecutionResult {
     pub exit_status: String,
     pub stdout: String,
